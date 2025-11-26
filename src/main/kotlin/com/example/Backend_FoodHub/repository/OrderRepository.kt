31addbbb0369e6394f6f29f9ac6c2cc286399fb8
MartinDev <marinmalvarez@gmail.com>
2025-11-26 00:24:10 -0300

    Se crean los repository

---
+++ src/main/kotlin/com/example/Backend_FoodHub/repository/OrderRepository.kt
@@ -0,0 +1,10 @@
+package com.example.backend_foodhub.repository
+
+import com.example.backend_foodhub.model.Order
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface OrderRepository : JpaRepository<Order, Long> {
+    fun findByUserId(userId: Long): List<Order>
+}
---
+++ src/main/kotlin/com/example/Backend_FoodHub/repository/ProductRepository.kt
@@ -0,0 +1,7 @@
+package com.example.backend_foodhub.repository
+import com.example.backend_foodhub.model.Product
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ProductRepository : JpaRepository<Product, Long>
