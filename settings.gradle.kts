a26f86dac1c75694cacdf4878efda43b1ba75c29
MartinDev <marinmalvarez@gmail.com>
2025-11-26 00:51:34 -0300

    se modifica settings.gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,13 @@
-rootProject.name = "Backend_FoodHub"
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+plugins {
+    // Este plugin permite a Gradle descargar Java automáticamente
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+rootProject.name = "backend-foodhub"
