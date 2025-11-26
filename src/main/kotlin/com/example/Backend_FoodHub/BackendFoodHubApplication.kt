f90ff9eab2d1f35da2c69ffabfc502acda15eac8
MartinDev <marinmalvarez@gmail.com>
2025-11-26 00:02:38 -0300

    First commit

---
+++ src/main/kotlin/com/example/Backend_FoodHub/BackendFoodHubApplication.kt
@@ -0,0 +1,11 @@
+package com.example.Backend_FoodHub
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendFoodHubApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendFoodHubApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/Backend_FoodHub/BackendFoodHubApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.Backend_FoodHub
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendFoodHubApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
